10801bd62379b57ebf00d35fe34db5d0ccc31553
Perevalov1 <danya.perevalov@yandex.ru>
2016-11-26 17:33:40 +0800

    Update PrANDCrug.cpp

--- PrANDCrug.cpp
+++ PrANDCrug.cpp
@@ -1,25 +1,40 @@
-# include <iostream>
+#include <iostream>
+#include <locale.h>
+#include <conio.h>
 using namespace std;
-class Pr{public:
-int a,b;
-	int r;
-	int gets() {return a*b;}
-	int getp() {return 2*(a+b);}
-	int getk() {return 3,14*r*r;}
-	int geth() {return 2*3,14*r;}
+class PR 
+{	int a, b;
+
+public:  PR  (int l, int r) {
+			a = l;
+			b = r;
+		}
+		int getS() { return a*b; };
+		int getP() { return 2*(a+b);}
 };
-	void main()
-	{
-	Pr p;
-    p.a=8;
-	p.b=10;
-	p.r=3;
-	cout<<p.gets ();
-	cout<<" ";
-	cout<<p.getp ();
-	cout<<" ";
-    cout<<p.getk ();
-	cout<<" ";
-	cout<<p.geth ();
+
+class Crug
+{	int p;
+public: Crug (int e) {
+			p = e;
+		}
+		int getS() {return 3.14*p*p;};
+		int getP() {return 2*3.14*p;};
+};
+void main() {
+	setlocale(LC_ALL, "RUS"); 
+	int c, d, m;
+	cout << "Ââåäèòå ñòîðîíó À: ";
+	cin >> c;
+	cout << "Ââåäèòå ñòîðîíó B: ";
+	cin >> d;
+	Square a(c, d);
+	cout << "Ïëîùàäü ïðÿìîóãîëüíèêà: " << a.getS() << endl;
+	cout << "Ïåðèìåòð ïðÿìîóãîëüíèêà: " << a.getP() << endl;
+	cout << "Ââåäèòå ðàäèóñ êðóãà: ";
+	cin >> m;
+	Circle p(m);
+	cout << "Ïëîùàäü êðóãà: " << p.getS() << endl;
+	cout << "Ïåðèìåòð êðóãà: " << p.getP() << endl;
 	system ("pause");
 }
